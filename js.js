c71e6a66a08553d74d4a7b1aa59b8d2cb755f2e3
phu <votanphu150102@gmail.com>
2022-10-13 16:23:19 +0700

    tanphune

---
+++ js.js
@@ -0,0 +1,16 @@
+// // $('.search-button').click(function(){
+// //     $(this).parent().toggleClass('open');
+// //   });
+
+//   let hieuung = document.querySelectorAll('.hieuung');
+//   document.addEventListener('scroll',(event)=>){
+//     hieuung.forEach(hieuung =>){
+//         if(hieuung.offsetTop -window.scrollY<350){
+//             hieuung.classList.add('active');
+//         }
+//     }
+//   }
+
+// đăng  nhập đăng kí
+var tenDN = document.querySelector(".form-phu");
+console.log(tenDN);
